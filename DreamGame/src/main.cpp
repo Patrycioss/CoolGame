4b9589331593dc231debaede3e59f6654ad46ad3
Patrick Schuur <patrickschuurdev@gmail.com>
2024-10-25 23:23:32 +0200

    Changed the main loop to work better on web

--- DreamGame/src/main.cpp
+++ DreamGame/src/main.cpp
@@ -6,22 +6,44 @@
 
 #include "raylib.h"
 
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+static Texture2D texture;
+
+static void UpdateDrawFrame() {
+	BeginDrawing();
+	DrawTexture(texture, 0,0,WHITE);
+	ClearBackground(RED);
+	DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+	EndDrawing();
+}
+
+constexpr int TARGET_FPS = 60;
+
+
 int main() {
 	
 	InitWindow(800, 450, "raylib [core] example - basic window");
+	InitAudioDevice();
 
 	std::string path = std::string(RESOURCES) + "/textures/awesomeface.png";
-	const Texture2D texture = LoadTexture(path.c_str());
+	texture = LoadTexture(path.c_str());
 
+#if defined(PLATFORM_WEB)
+	emscripten_set_main_loop(UpdateDrawFrame,TARGET_FPS,1);
+#else
+
+	SetTargetFPS(TARGET_FPS);
+	
 	while (!WindowShouldClose())
 	{
-		BeginDrawing();
-		DrawTexture(texture, 0,0,WHITE);
-		ClearBackground(RED);
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-		EndDrawing();
+		UpdateDrawFrame();
 	}
+#endif
 
+	CloseAudioDevice();
 	CloseWindow();
 
 	return 0;
