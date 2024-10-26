eddcb70dedd69a9fb43dfd6ab87f624768ddf38f
Patrick Schuur <patrickschuurdev@gmail.com>
2024-10-26 17:16:57 +0200

    Get rid of update docs batch script

--- update_docs.bat
+++
@@ -1,8 +0,0 @@
-COPY DreamGame\cmake-build-build-web\DreamGame.data docs
-COPY DreamGame\cmake-build-build-web\DreamGame.js docs
-COPY DreamGame\cmake-build-build-web\DreamGame.wasm docs
-COPY DreamGame\cmake-build-build-web\DreamGame.html docs
-DEL docs\old.html
-RENAME docs\index.html old.html
-RENAME docs\DreamGame.html index.html
-pause
