38b41e85cf16f7b234473f6db73b0138301ace0d
Patrick Schuur <patrickschuurdev@gmail.com>
2024-10-26 17:59:13 +0200

    Moved texture initialization to initializer list

--- DreamGame/src/Game.cpp
+++ DreamGame/src/Game.cpp
@@ -1,17 +1,14 @@
 ﻿#include "Game.hpp"
 
-#include <raylib.h>
 #include <string>
 
-Game::Game() {
-	const std::string path = std::string(RESOURCES) + "/textures/awesomeface.png";
-	texture = LoadTexture(path.c_str());
+Game::Game()
+	: texture(LoadTexture((std::string(RESOURCES) + "/textures/awesomeface.png").c_str())) {
 }
 
 void Game::Update() {
-
 	BeginDrawing();
-	DrawTexture(texture, 0,0,WHITE);
+	DrawTexture(texture, 0, 0,WHITE);
 	ClearBackground(RED);
 	DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 	EndDrawing();
