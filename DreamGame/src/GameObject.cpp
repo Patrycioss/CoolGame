6ba9f2c452718e4c34a88546eb381f9fa9953fbf
Patrick Schuur <patrickschuurdev@gmail.com>
2024-10-27 21:20:10 +0100

    Added GameObject and Transform2D

---
+++ DreamGame/src/GameObject.cpp
@@ -0,0 +1,38 @@
+﻿#include "GameObject.hpp"
+
+GameObject::GameObject(const Vector2& position)
+	: transform(position) {
+}
+
+void GameObject::Update() {
+}
+
+void GameObject::Render() {
+}
+
+Vector2& GameObject::position(const Vector2& newValue) {
+	transform.position = newValue;
+	return transform.position;
+}
+
+Vector2& GameObject::position() {
+	return transform.position;
+}
+
+Vector2& GameObject::scale(const Vector2& newValue) {
+	transform.scale = newValue;
+	return transform.scale;
+}
+
+Vector2& GameObject::scale() {
+	return transform.scale;
+}
+
+float& GameObject::rotation(const float& newValue) {
+	transform.rotation = newValue;
+	return transform.rotation;
+}
+
+float& GameObject::rotation() {
+	return transform.rotation;
+}
---
+++ DreamGame/src/GameObject.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "Transform2D.hpp"
+
+class GameObject {
+	private:
+		Transform2D transform;
+
+	public:
+		virtual ~GameObject() = default;
+		explicit GameObject(const Vector2& position);
+		virtual void Update();
+		virtual void Render();
+
+		Vector2& position(const Vector2& newValue);
+		Vector2& position();
+
+		Vector2& scale(const Vector2& newValue);
+		Vector2& scale();
+
+		float& rotation(const float& newValue);
+		float& rotation();
+};
