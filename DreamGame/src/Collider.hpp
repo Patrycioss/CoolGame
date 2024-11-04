0d3b6cfc48dddfcec4a1e8b6e478e25b94a91dec
Patrick Schuur <patrickschuurdev@gmail.com>
2024-11-04 18:48:45 +0100

    Added debug render function to collider

--- DreamGame/src/Collider.hpp
+++ DreamGame/src/Collider.hpp
@@ -14,7 +14,7 @@ class Collider {
 		};
 
 	private:
-		GameObject& gameObject;
+		GameObject& owner;
 		const Type type;
 
 	public:
@@ -23,58 +23,20 @@ class Collider {
 
 		[[nodiscard]] const Type& Type() const;
 
-		float width;
-		float height;
+		float width{};
+		float height{};
 		float radius{};
 		[[nodiscard]] GameObject& Owner() const;
 
+		void DebugRender(const Color& color) const;
+
 	private:
-		static bool RectRectOverlaps(const Collider& first, const Collider& second) {
-			return second.Owner().position.x < first.Owner().position.x + first.width &&
-				second.Owner().position.y + second.width >
-				first.Owner().position.x &&
-				second.Owner().position.y < first.Owner().position.y + first.height &&
-				second.Owner().position.y + second.height >
-				first.Owner().position.y;
-		}
+		static bool RectRectOverlaps(const Collider& first, const Collider& second);
 
-		static bool CircleCircleOverlaps(const Collider& first, const Collider& second) {
-			return Vector2Distance(first.Owner().position, second.Owner().position) > first.radius + second.radius;
-		}
+		static bool CircleCircleOverlaps(const Collider& first, const Collider& second);
 
-		static bool CircleRectOverlaps(const Collider& circle, const Collider& rect) {
-			const auto [circleX, circleY] = circle.Owner().position;
-			const auto [rectX, rectY] = rect.Owner().position;
-
-			float testX = circleX;
-			float testY = circleY;
-
-			if (circleX < rectX) testX = rectX; // left edge
-			else if (circleX > rectX + rect.width) testX = rectX + rect.width; // right edge
-
-			if (circleY < rectY) testY = rectY; // top edge
-			else if (circleY > rectY + rect.height) testY = rectY + rect.height; // bottom edge
-
-			float distX = circleX - testX;
-			float distY = circleY - testY;
-			float sqDistance = (distX * distX) + (distY * distY);
-
-			return sqDistance <= (circle.radius * circle.radius);
-		}
+		static bool CircleRectOverlaps(const Collider& circle, const Collider& rect);
 
 	public:
-		static bool Overlaps(const Collider& first, const Collider& second) {
-			if (first.Type() == Type::Rectangle && second.Type() == Type::Rectangle) {
-				return RectRectOverlaps(first, second);
-			}
-
-			if (first.Type() == Type::Circle && second.Type() == Type::Circle) {
-				return CircleCircleOverlaps(first, second);
-			}
-
-			if (first.Type() == Type::Circle && second.Type() == Type::Rectangle) return CircleRectOverlaps(first, second);
-			if (first.Type() == Type::Rectangle && second.Type() == Type::Circle) return CircleRectOverlaps(second, first);
-
-			throw std::runtime_error("Failed to implement collision between certain types?");
-		}
+		static bool Overlaps(const Collider& first, const Collider& second);
 };
