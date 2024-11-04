c0498adf2fd7d2dc90ef8db246c4d3dc4f2c43eb
Patrick Schuur <patrickschuurdev@gmail.com>
2024-11-04 17:06:45 +0100

    Added collider class

---
+++ DreamGame/src/Collider.cpp
@@ -0,0 +1,7 @@
+﻿#include "Collider.hpp"
+
+Collider::Collider(const GameObject* gameObject, const float radius): gameObject(gameObject), type(Type::Circle), radius(radius) {
+}
+
+Collider::Collider(const GameObject* gameObject, const Vector2& size): gameObject(gameObject), type(Type::Rectangle), size(size) {
+}
---
+++ DreamGame/src/Collider.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "objects/GameObject.hpp"
+
+class Collider {
+	public:
+		enum class Type {
+			Rectangle,
+			Circle,
+		};
+
+	private:
+		const GameObject* gameObject;
+		const Type type;
+
+		Vector2 size{};
+		float radius{};
+
+	public:
+		explicit Collider(const GameObject* gameObject, float radius);
+		explicit Collider(const GameObject* gameObject, const Vector2& size);
+};
