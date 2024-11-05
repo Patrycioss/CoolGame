8163600d8186d3b7347c18918fd38a329a618922
Patrick Schuur <patrickschuurdev@gmail.com>
2024-11-05 16:24:01 +0100

    Added vector2 operator extensions

---
+++ DreamGame/src/engine/utils/Vector2Extensions.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <raylib.h>
+
+inline Vector2 operator+(const Vector2& lhs, const Vector2& rhs) {
+	return Vector2(lhs.x + rhs.x, lhs.y + rhs.y);
+}
+
+inline Vector2 operator-(const Vector2& lhs, const Vector2& rhs) {
+	return Vector2(lhs.x - rhs.x, lhs.y - rhs.y);
+}
+
+inline Vector2 operator*(const Vector2& lhs, const Vector2& rhs) {
+	return Vector2(lhs.x * rhs.x, lhs.y * rhs.y);
+}
+
+inline Vector2 operator*(const float lhs, const Vector2& rhs) {
+	return Vector2(lhs * rhs.x, lhs * rhs.y);
+}
+
+inline Vector2 operator*(const Vector2& lhs, const float rhs) {
+	return Vector2(lhs.x * rhs, lhs.y * rhs);
+}
+
+inline Vector2 operator/(const Vector2& lhs, const float rhs) {
+	return Vector2(lhs.x / rhs, lhs.y / rhs);
+}
